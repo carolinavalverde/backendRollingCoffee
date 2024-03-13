00071e6554d232d6df4efd5caa758542f537d32d
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-13 18:36:40 -0300

    agregamos un schema de producto

---
+++ src/database/models/producto.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 100, //prueba para ver si funciona con valor negativo, si no lo ponemos si dejaria
+    max: 10000,
+  },
+  imagen: {
+    type: String,
+    requiered: true,
+    validate: {
+      validator: (dato) => {
+        const pattern = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)$/;
+        return pattern.test(dato);
+      },
+    },
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  descripcion_breve: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 50,
+  },
+  descripcion_amplia: {
+    type: String,
+    required: true,
+    minLength: 30,
+    maxLength: 300,
+  },
+});
