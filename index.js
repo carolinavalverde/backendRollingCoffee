9db9694f25e7ed41055d56f6542481a2548383ab
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-13 16:55:39 -0300

    agregamos el middleword para cargar la ruta ppal en el servidor

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import 'dotenv/config'; //permite procesar variable de entorno
 import cors from 'cors';
 import morgan from 'morgan';
 import productosRouter from './src/routes/productos.routes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 //1- configurar un puerto
 const app = express();
@@ -17,10 +19,16 @@ app.use(morgan('dev')); //nos da informacion extra en la terminal
 app.use(express.json()); //permite interpretar los datos en formato json
 app.use(express.urlencoded({extended:true})); //ayuda a interpretar los datos del body del request
 // todo: configurar index.html
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//console.log(__filename)
+//console.log(__dirname)
+app.use(express.static(path.join(__dirname, "/public")))
 
 //3- configurar las rutas
 // app.get("/nuevo/producto", (req, res) => {
 //     console.log("Aquí obtener la lista de todos los productos");
 //     res.send("Aqui enviaremos la lista de productos")
 // });
+//http://localhost:4001/api/nuevo/producto
 app.use("/api", productosRouter)
