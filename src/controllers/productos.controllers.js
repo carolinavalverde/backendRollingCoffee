997395f57b077f6f69d70eb1e6ec0f2059064bdf
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-18 18:20:05 -0300

    agregamos logica para obtener prducto por id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,14 +1,33 @@
 import Producto from "../database/models/producto.js";
 
-export const listarProductos = async(req, res) => {
+export const listarProductos = async (req, res) => {
   try {
     //pedir a la BD la lista de todos los productos
-    const productos = await Producto.find()
+    const productos = await Producto.find();
     //responder al frontend con el array de productos
-    res.status(200).json(productos)
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({mensaje: "Error al buscar los productos"})
+    res.status(200).json(productos);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ mensaje: "Error al buscar los productos" });
+  }
+};
+
+export const obtenerProducto = async (req, res) => {
+  try {
+    //verificar si el producto existe con el id correspondiente
+    console.log(req.params.id);
+    const productoBuscado = await Producto.findById(req.params.id);
+    //si no existe contestar con un status404 not found
+    if (!productoBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "El id enviado no corresponde a ningun producto" });
+    }
+    //si exoste el producto eviarlo al frontend, status200
+    res.status(200).json(productoBuscado);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ mensaje: "Error al obtener el producto" });
   }
 };
 
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { crearProducto, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProducto, listarProductos, obtenerProducto } from "../controllers/productos.controllers.js";
 
 const router = Router();
 
@@ -9,5 +9,6 @@ const router = Router();
 // });
 
 router.route("/productos").get(listarProductos).post(crearProducto)
+router.route("/productos/:id").get(obtenerProducto)
 
 export default router
