dccbd2a59b54392ce7355c94f31dd1bf09d5460e
Caro Valverde <carolina.valverde255@gmail.com>
2024-03-18 18:36:21 -0300

    agregamos logica para editar un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,5 +1,23 @@
 import Producto from "../database/models/producto.js";
 
+export const crearProducto = async (req, res) => {
+  try {
+    //todo: validar los datos del body
+    //crear un producto basado en el mpdelo producto
+    const productoNuevo = new Producto(req.body);
+    //pedir a la BD crear el prdicto
+    await productoNuevo.save();
+    //enviar una respuesta cuando puedo crear efectivamente el producto 201
+    res.status(201).jason({ mensaje: "El producto fue creado correctamente" });
+  } catch (error) {
+    //enviar una respuesta de error
+    console.error(error);
+    res.status(500).json({
+      mensaje: "Error al crear el producto",
+    });
+  }
+};
+
 export const listarProductos = async (req, res) => {
   try {
     //pedir a la BD la lista de todos los productos
@@ -23,7 +41,7 @@ export const obtenerProducto = async (req, res) => {
         .status(404)
         .json({ mensaje: "El id enviado no corresponde a ningun producto" });
     }
-    //si exoste el producto eviarlo al frontend, status200
+    //si existe el producto eviarlo al frontend, status200
     res.status(200).json(productoBuscado);
   } catch (err) {
     console.error(err);
@@ -31,20 +49,23 @@ export const obtenerProducto = async (req, res) => {
   }
 };
 
-export const crearProducto = async (req, res) => {
+export const editarProducto = async (req, res) => {
   try {
     //todo: validar los datos del body
-    //crear un producto basado en el mpdelo producto
-    const productoNuevo = new Producto(req.body);
-    //pedir a la BD crear el prdicto
-    await productoNuevo.save();
-    //enviar una respuesta cuando puedo crear efectivamente el producto 201
-    res.status(201).jason({ mensaje: "El producto fue creado correctamente" });
-  } catch (error) {
-    //enviar una respuesta de error
-    console.error(error);
-    res.status(500).json({
-      mensaje: "Error al crear el producto",
-    });
+    //verificar si el producto existe con el id correspondiente
+    console.log(req.params.id);
+    const productoBuscado = await Producto.findById(req.params.id);
+    //si no existe contestar con un status404 not found
+    if (!productoBuscado) {
+      return res
+        .status(404)
+        .json({ mensaje: "El id enviado no corresponde a ningun producto" });
+    }
+    //modificar el producto y enviar la respuesta 400
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ mensaje: "El producto fue editado correctamente" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ mensaje: "Error al editar el producto" });
   }
 };
