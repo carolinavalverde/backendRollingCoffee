e88d36a436a5e80a9a874f8c8a5fafc14b519b22
Caro Valverde <carolina.valverde255@gmail.com>
2024-04-08 19:28:30 -0300

    agrego solicitud de get en routes

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,3 +1,32 @@
+// import { Router } from "express";
+// import {
+//   borrarProducto,
+//   crearProducto,
+//   editarProducto,
+//   listarProductos,
+//   obtenerProducto,
+// } from "../controllers/productos.controllers.js";
+// import { check } from "express-validator";
+// import validacionProducto from "../helpers/validacionProducto.js";
+
+// const router = Router();
+
+// // app.get('/nuevo/producto',(req, res)=>{
+// //     console.log('aqui obtener la lista de todos los productos');
+// //     res.send('Aqui enviaremos la lista de productos')
+// //    })
+// router
+//   .route("/productos")
+//   .get(listarProductos)
+//   .post([validacionProducto],crearProducto);
+// router
+//   .route("/productos/:id")
+//   .get(obtenerProducto)
+//   .put(editarProducto)
+//   .delete(borrarProducto);
+
+// export default router;
+
 import { Router } from "express";
 import {
   borrarProducto,
@@ -11,14 +40,16 @@ import validacionProducto from "../helpers/validacionProducto.js";
 
 const router = Router();
 
-// app.get('/nuevo/producto',(req, res)=>{
-//     console.log('aqui obtener la lista de todos los productos');
-//     res.send('Aqui enviaremos la lista de productos')
-//    })
+router.get('/', (req, res) => {
+  console.log('Aquí se obtendrá la lista de todos los productos');
+  res.send('Aquí enviaremos la lista de productos');
+});
+
 router
   .route("/productos")
   .get(listarProductos)
-  .post([validacionProducto],crearProducto);
+  .post([validacionProducto], crearProducto);
+
 router
   .route("/productos/:id")
   .get(obtenerProducto)
